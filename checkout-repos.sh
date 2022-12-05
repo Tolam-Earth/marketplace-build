7419d0da221e56fa53e4e9c8274e733c194793f0
Tolam-Automation <117318520+Tolam-Automation@users.noreply.github.com>
2022-12-05 11:53:27 -0600

    Update checkout-repos.sh

--- checkout-repos.sh
+++ checkout-repos.sh
@@ -4,5 +4,5 @@ set -e
 rm -rf build-repos
 mkdir -p build-repos
 
-git clone git@github.com:Tolam-Earth/Marketplace-UI.git -b demo build-repos/ui
-git clone git@github.com:Tolam-Earth/Marketplace-Services.git -b demo build-repos/services
+git clone git@github.com:Tolam-Earth/marketplace-ui.git -b demo build-repos/ui
+git clone git@github.com:Tolam-Earth/marketplace-services.git -b demo build-repos/services
